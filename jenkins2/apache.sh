829450d55b160b3abd7b1406f197e91bd987a5ad
jwcho <djdlzl93@gmail.com>
2021-11-04 16:35:43 +0900

    test3

--- jenkins2/apache.sh
+++ jenkins2/apache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum install -y httpd
-cat > /var/www/html/index.html << EOC
+cat > /var/www/html/index.html << EOF
 <html><body><h1>aws-jenkins-collabo</h1></body></html>
+EOF
 systemctl start httpd
